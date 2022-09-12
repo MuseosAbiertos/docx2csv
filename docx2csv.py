a0b5766fa0f50ee5358130d9ec03778a651f763d
marting <mg@museosabiertos.org>
2022-09-12 10:32:52 -0300

    minor text changes

--- docx2csv.py
+++ docx2csv.py
@@ -3,7 +3,10 @@ version 0.2
 date 2022-05-14
 Martin Gersbach [mg@museosabiertos.org]
 
-docx2csv.py parses .docx files to extract artwork information, then attempts to pair them up with image files residing in the same directories via filename similarity. It finally outputs a CSV associating each image file to the data parsed from the .docx sharing its name.
+docx2csv.py parses .docx files to extract artwork information, then attempts to
+pair them up with image files residing in the same directories via filename similarity. 
+Extract the following tags VRA Core: 'Work Agent', 'Work Title', 'Work ID', 'Work Type', 'Work Description', 'Work Measurements', 'Work Date'
+It finally outputs a CSV associating each image file to the data parsed from the .docx sharing its name.
 
 It can be run using: "python docx2csv.py ROOT_PATH", or, alternatively the path will be asked for at runtime.
 
@@ -17,10 +20,14 @@ root_path
         - file 1
         - file 2
 --
-docx2csv.py analiza los archivos .docx para extraer la información de las ilustraciones, y luego intenta emparejarlos con los archivos de imagen
-que residen en los mismos directorios a través de la similitud de los nombres de los archivos. Finalmente, genera un CSV que asocia cada archivo de imagen a los datos analizados del .docx que comparte su nombre.
+docx2csv.py analiza los archivos .docx para extraer la información de las ilustraciones,
+y luego intenta emparejarlos con los archivos de imagen que residen en los mismos directorios
+a través de la similitud de los nombres de los archivos. 
+Extrae los siguientes tags VRA Core: 'Work Agent', 'Work Title', 'Work ID', 'Work Type', 'Work Description', 'Work Measurements', 'Work Date'
+Finalmente, genera un CSV que asocia cada archivo de imagen a los datos analizados del .docx que comparte su nombre.
 
-Se puede ejecutar utilizando: "python docx2csv.py ROOT_PATH", o, alternativamente, se pedirá la ruta en tiempo de ejecución.
+Se puede ejecutar utilizando: "python docx2csv.py ROOT_PATH", o, alternativamente, 
+se pedirá la ruta en tiempo de ejecución.
 
 El script espera que la estructura de directorios sólo tenga un nivel de profundidad, es decir, que sea:
 x
